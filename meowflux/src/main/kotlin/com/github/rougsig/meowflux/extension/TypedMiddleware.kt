c7964ccc9a0217180288d548d17e56f8c680bd00
rougsig <rougsig@gmail.com>
2020-02-09 01:25:27 +0200

    Fix TypedMiddleware again

--- meowflux/src/main/kotlin/com/github/rougsig/meowflux/extension/TypedMiddleware.kt
+++ meowflux/src/main/kotlin/com/github/rougsig/meowflux/extension/TypedMiddleware.kt
@@ -5,7 +5,7 @@ import com.github.rougsig.meowflux.core.Dispatcher
 import com.github.rougsig.meowflux.core.Middleware
 
 inline fun <reified A : Action, S : Any> createTypedMiddleware(
-  crossinline middleware: suspend (action: A, root: Dispatcher, state: () -> S, next: Dispatcher) -> S
+  crossinline middleware: suspend (action: A, root: Dispatcher, state: () -> S, next: Dispatcher) -> Unit
 ): Middleware<S> = { root, state, next ->
   { action ->
     if (action is A) middleware(action, root, state, next)

cda616ebb4e6809ee81b549b6f7da276c8031f26
rougsig <rougsig@gmail.com>
2020-02-09 01:18:37 +0200

    Add TypedMiddleware

---
+++ meowflux/src/main/kotlin/com/github/rougsig/meowflux/extension/TypedMiddleware.kt
@@ -0,0 +1,14 @@
+package com.github.rougsig.meowflux.extension
+
+import com.github.rougsig.meowflux.core.Action
+import com.github.rougsig.meowflux.core.Dispatcher
+import com.github.rougsig.meowflux.core.Middleware
+
+inline fun <reified A : Action, S : Any> createTypedMiddleware(
+  crossinline middleware: (action: A, root: Dispatcher, state: () -> S, next: Dispatcher) -> S
+): Middleware<S> = { root, state, next ->
+  { action ->
+    if (action is A) middleware(action, root, state, next)
+    else next(action)
+  }
+}

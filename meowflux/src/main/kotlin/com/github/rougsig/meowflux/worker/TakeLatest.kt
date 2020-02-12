172e9c30200f6759ed3723b3d76997d0c523df21
st <st@appkode.ru>
2020-02-12 15:29:16 +0200

    Add takeLatest

---
+++ meowflux/src/main/kotlin/com/github/rougsig/meowflux/worker/TakeLatest.kt
@@ -0,0 +1,32 @@
+package com.github.rougsig.meowflux.worker
+
+import com.github.rougsig.meowflux.core.Action
+import com.github.rougsig.meowflux.core.Dispatcher
+import com.github.rougsig.meowflux.core.Middleware
+import com.github.rougsig.meowflux.core.createStore
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flow
+import kotlin.coroutines.coroutineContext
+import kotlin.coroutines.intrinsics.suspendCoroutineUninterceptedOrReturn
+
+inline fun <reified A : Action, S : Any> takeLatest(
+  timeoutMillis: Long,
+  crossinline worker: suspend WorkerContext<S>.(action: A) -> Unit
+): Middleware<S> = { root, getState, next ->
+  var job: Job? = null
+  val context = WorkerContext(root, getState)
+  val dispatcher: Dispatcher = { action ->
+    if (action is A) {
+      job?.cancel()
+      coroutineScope {
+        job = async {
+          delay(timeoutMillis)
+          context.worker(action)
+        }
+      }
+    } else next(action)
+  }
+  dispatcher
+}
--- meowflux/src/test/kotlin/com/github/rougsig/meowflux/core/fakes/DublicateMiddleware.kt
+++ meowflux/src/test/kotlin/com/github/rougsig/meowflux/core/fakes/DublicateMiddleware.kt
@@ -1,6 +1,10 @@
 package com.github.rougsig.meowflux.core.fakes
 
 import com.github.rougsig.meowflux.core.Middleware
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import kotlin.coroutines.coroutineContext
 
 val duplicateMiddleware: Middleware<CatCounter> = { _, _, next ->
   { action ->

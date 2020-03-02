921a084068635d066196869ead981039c5bae24c
rougsig <rougsig@gmail.com>
2020-03-02 21:23:32 +0200

    Add View connect

---
+++ meowflux-android/src/main/java/com/github/rougsig/meowflux/android/FlowExtensions.kt
@@ -0,0 +1,28 @@
+package com.github.rougsig.meowflux.android
+
+import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+fun <S : Any> Flow<S>.observe(view: View, init: StateDiff<S>.() -> Unit) {
+  view.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+    private var scope: CoroutineScope? = null
+    private val diff = StateDiff<S>().also(init)
+    override fun onViewDetachedFromWindow(v: View?) {
+      scope = MainScope().also { scope ->
+        scope.launch {
+          this@observe.collect { diff.update(it) }
+        }
+      }
+    }
+
+    override fun onViewAttachedToWindow(v: View?) {
+      scope?.cancel()
+      scope = null
+    }
+  })
+}
---
+++ meowflux-android/src/main/java/com/github/rougsig/meowflux/android/ViewConnect.kt
@@ -0,0 +1,38 @@
+package com.github.rougsig.meowflux.android
+
+import android.content.Context
+import android.view.View
+import com.github.rougsig.meowflux.core.StoreDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+fun <V : View, S : Any, SP : Any, DP : Any> connect(
+  viewConstructor: (Context, Flow<SP>, DP) -> V,
+  mapStateToProps: (S) -> SP,
+  mapDispatchToProps: (StoreDispatcher) -> DP
+): (Context) -> V {
+  return { context ->
+    val store = context.store<S>()
+    viewConstructor(
+      context,
+      store.stateFlow.map { mapStateToProps(it) },
+      mapDispatchToProps(store)
+    )
+  }
+}
+
+fun <V : View, S : Any, SP : Any, DP : Any, OP : Any> connect(
+  viewConstructor: (Context, Flow<SP>, DP, OP) -> V,
+  mapStateToProps: (S) -> SP,
+  mapDispatchToProps: (StoreDispatcher) -> DP
+): (Context, ownProps: OP) -> V {
+  return { context, ownProps ->
+    val store = context.store<S>()
+    viewConstructor(
+      context,
+      store.stateFlow.map { mapStateToProps(it) },
+      mapDispatchToProps(store),
+      ownProps
+    )
+  }
+}

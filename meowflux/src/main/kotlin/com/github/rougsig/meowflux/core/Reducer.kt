10819c0efe8a05aab738ded3ba1a6bcd9a420103
rougsig <rougsig@gmail.com>
2020-03-06 22:59:54 +0200

    Move TypedReducer to Reducer

--- meowflux/src/main/kotlin/com/github/rougsig/meowflux/core/Reducer.kt
+++ meowflux/src/main/kotlin/com/github/rougsig/meowflux/core/Reducer.kt
@@ -1,3 +1,13 @@
 package com.github.rougsig.meowflux.core
 
 typealias Reducer<S> = (action: Action, previousState: S?) -> S
+
+@Suppress("FunctionName")
+inline fun <reified A : Action, S : Any> TypedReducer(
+  initialState: S,
+  crossinline reducer: (action: A, previousState: S) -> S
+): Reducer<S> = { action, previousState ->
+  val state = previousState ?: initialState
+  if (action is A) reducer(action, state)
+  else state
+}
